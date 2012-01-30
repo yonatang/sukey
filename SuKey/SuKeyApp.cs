8d23bd79fbdd2baa24f4e68c0805a09fad301e38
Yonatan Graber <yonatan.graber@gmail.com>
2012-01-30 17:24:32 +0200

    Initial commit

---
+++ SuKey/SuKeyApp.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SuKey
+{
+    public class SuKeyApp : Form
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new SuKeyApp());
+        }
+
+        private NotifyIcon trayIcon;
+        private ContextMenu trayMenu;
+        private KeyboardHook hook;
+
+        public SuKeyApp()
+        {
+            // Create a tray menu
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Switch (Win+S)", OnSwitch);
+            trayMenu.MenuItems.Add("-");
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            // Create a tray icon
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "SuKey\nDoubleclick to switch user";
+            trayIcon.MouseDoubleClick += new MouseEventHandler(OnSwitch);
+
+            // Use a custom icon
+            Icon i = new Icon(typeof(SuKeyApp), "icon.ico");
+            trayIcon.Icon = i;
+
+            // Register a global hot key
+            hook = new KeyboardHook();
+            hook.KeyPressed += new EventHandler<KeyPressedEventArgs>(OnSwitch);
+
+            hook.RegisterHotKey(Properties.Settings.Default.Modifier, Properties.Settings.Default.Key);
+
+            // Add menu to tray icon and show it.
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false; // Hide form window.
+            ShowInTaskbar = false; // Remove from taskbar.
+
+            base.OnLoad(e);
+        }
+
+        private void OnSwitch(object sender, EventArgs e)
+        {
+            Switcher.Switch();
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                trayIcon.Dispose();
+            }
+
+            base.Dispose(isDisposing);
+        }
+
+    }
+}
---
+++ SuKey/Switcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+
+namespace SuKey
+{
+    class Switcher
+    {
+        //Switch method
+        [DllImport("wtsapi32.dll", SetLastError = true)]
+        static extern bool WTSDisconnectSession(IntPtr hServer, int sessionId, bool bWait);
+        
+        const int WTS_CURRENT_SESSION = -1;
+        static readonly IntPtr WTS_CURRENT_SERVER_HANDLE = IntPtr.Zero;
+
+        public static void Switch()
+        {
+            if (!WTSDisconnectSession(WTS_CURRENT_SERVER_HANDLE, WTS_CURRENT_SESSION, false))
+                throw new Win32Exception();
+
+        }
+
+    }
+}
